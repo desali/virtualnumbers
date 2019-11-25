6c992408d7df7ade40e6d12e3356f2e1963dd64a
Sultan <sultansailaubaev1998@gmail.com>
2019-11-26 01:01:51 +0500

    Aded home, about, faq components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
+import { AboutComponent } from './about/about.component';
+import { CountryComponent } from './country/country.component';
+import { NumberComponent } from './number/number.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'countries',
+    component: CountryComponent
+  },
+  {
+    path: 'number/:id',
+    component: NumberComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './_templates/header/header.component';
+import { FooterComponent } from './_templates/footer/footer.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FaqComponent } from './faq/faq.component';
+import { AboutComponent } from './about/about.component';
+import { CountryComponent } from './country/country.component';
+import { NumberComponent } from './number/number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FaqComponent,
+    AboutComponent,
+    CountryComponent,
+    NumberComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
